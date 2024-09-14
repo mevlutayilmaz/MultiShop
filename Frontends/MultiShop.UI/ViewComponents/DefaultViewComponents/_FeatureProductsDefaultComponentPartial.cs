fceaea91cf4e1372db986283107bbdf10ec3cda6
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-09-14 18:50:57 +0300

    ASP.Net Core MVC user layout created

---
+++ Frontends/MultiShop.UI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.UI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.UI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.UI.ViewComponents.DefaultViewComponents
+{
+    public class _OfferDiscountDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
