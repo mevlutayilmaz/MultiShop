b3b334a763ec9e6e6133bab35a4ef0abd20cac7b
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-09-15 19:48:52 +0300

    Contact page created in frontend

---
+++ Frontends/MultiShop.UI/ViewComponents/ContactViewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.UI.ViewComponents.ContactViewComponents
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
