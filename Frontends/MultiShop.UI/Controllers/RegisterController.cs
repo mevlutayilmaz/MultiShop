6cbad93f4a30e4baf6d64c8ad3cb6bc256a4311d
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-09-18 17:49:24 +0300

    Login and Register operations have been completed

---
+++ Frontends/MultiShop.UI/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DTOLayer.IdentityDTOs.RegisterDTOs;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.UI.Controllers
+{
+    [AllowAnonymous]
+    [Route("Register")]
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Index")]
+        public async Task<IActionResult> Index(CreateRegisterDTO createRegisterDTO)
+        {
+            if(createRegisterDTO.Password == createRegisterDTO.ConfirmPassword)
+            {
+				var client = _httpClientFactory.CreateClient();
+				var jsonData = JsonConvert.SerializeObject(createRegisterDTO);
+				StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+				var responseMessage = await client.PostAsync("http://localhost:5001/api/Users", stringContent);
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+			}
+            return View();
+        }
+    }
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/AuthsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.DTOs;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+	//[Authorize(LocalApi.PolicyName)]
+	[AllowAnonymous]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthsController : ControllerBase
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AuthsController(SignInManager<ApplicationUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserLogin(UserLoginDTO userLoginDTO)
+		{
+			var result = await _signInManager.PasswordSignInAsync(userLoginDTO.Username, userLoginDTO.Password, false, false);
+			if (result.Succeeded) return Ok("Giriş Başarılı");
+			return Ok("Kullanıcı Adı veya Şifre Hatalı");
+		}
+	}
+}
