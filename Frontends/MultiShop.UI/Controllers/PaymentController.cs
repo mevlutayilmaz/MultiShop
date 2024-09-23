3dc5818d5805debede8bb74ccddd773e304c10a0
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-09-23 15:43:06 +0300

    UI payment operations

---
+++ Frontends/MultiShop.UI/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.UI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.dictionary1 = "Home";
+            ViewBag.dictionary2 = "Shop";
+            ViewBag.dictionary3 = "Payment";
+            ViewBag.dictionary1Url = "/Default/Index";
+            ViewBag.dictionary2Url = "/Default/Index";
+            return View();
+        }
+    }
+}
